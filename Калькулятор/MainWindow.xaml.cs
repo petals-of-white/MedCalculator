66a212dfbc8969c7c6edf629f13f4cd84995dbbf
Maksym <maxim.sivash@gmail.com>
2021-12-11 05:38:34 +0200

    add autocalculation

--- Калькулятор/MainWindow.xaml.cs
+++ Калькулятор/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Xceed.Wpf.Toolkit;
 
 namespace Калькулятор
 {
@@ -20,9 +12,162 @@ namespace Калькулятор
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<WatermarkTextBox> textBoxes;
         public MainWindow()
         {
             InitializeComponent();
+
+            textBoxes = new();
+
+            textBoxes.Add(CreaSyr);
+            textBoxes.Add(NaUrine);
+            textBoxes.Add(NaSyr);
+            textBoxes.Add(CreaUrine);
+
+
+            //foreach (WatermarkTextBox textBox in textBoxes)
+            //    textBox.KeyDown += CalculateOnKeyDown;
+
+
+        }
+
+
+
+        private void WatermarkTextBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+
+            int keyNumber = (int) e.Key;
+
+            if (((keyNumber >= 34) && (keyNumber <= 43))
+                   ||
+               ((keyNumber >= 74) && (keyNumber <= 83)))
+            {
+
+            }
+            else
+                e.Handled = true;
+
+
+        }
+
+        private void CalculateOnKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            var box = sender as WatermarkTextBox;
+            int keyNumber = (int) e.Key;
+
+            if (((keyNumber >= 34) && (keyNumber <= 43))
+                   ||
+               ((keyNumber >= 74) && (keyNumber <= 83)))
+            {
+                box.KeyDown += CalculateOnKeDownWow;
+                //int parseTemp;
+
+                //var valuesInBoxes = (from textbox in textBoxes
+                //                     select int.TryParse(textbox.Text, out parseTemp) ? parseTemp : -1).ToArray();
+
+
+                //if (!valuesInBoxes.Contains(-1))
+                //{
+                //    double fractExcretion = CalculateFractExcretion(
+                //        valuesInBoxes [0], valuesInBoxes [1], valuesInBoxes [2], valuesInBoxes [3]);
+
+                //    var interpreation = InterpretFractExcretion(fractExcretion);
+                //    Percentage.Inlines.Clear();
+                //    Percentage.Inlines.Add(fractExcretion + "%");
+
+                //    Diagnosis.Inlines.Clear();
+                //    Diagnosis.Inlines.Add(interpreation.Item1);
+
+                //    Explanation.Inlines.Clear();
+                //    Explanation.Inlines.Add(interpreation.Item2);
+                //}
+
+
+            }
+
+        }
+
+        private void CalculateOnKeDownWow(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            int keyNumber = (int) e.Key;
+
+
+            int parseTemp;
+
+            var valuesInBoxes = (from textbox in textBoxes
+                                 select int.TryParse(textbox.Text, out parseTemp) ? parseTemp : -1).ToArray();
+
+
+            if (!valuesInBoxes.Contains(-1))
+            {
+                double fractExcretion = CalculateFractExcretion(
+                    valuesInBoxes [0], valuesInBoxes [1], valuesInBoxes [2], valuesInBoxes [3]);
+
+                var interpreation = InterpretFractExcretion(fractExcretion);
+                Percentage.Inlines.Clear();
+                Percentage.Inlines.Add(fractExcretion + "%");
+
+                Diagnosis.Inlines.Clear();
+                Diagnosis.Inlines.Add(interpreation.Item1);
+
+                Explanation.Inlines.Clear();
+                Explanation.Inlines.Add(interpreation.Item2);
+
+
+
+            }
+        }
+
+        private void CalculateOnTextChange(object sender, TextChangedEventArgs e)
+        {
+
+            int parseTemp;
+
+            var valuesInBoxes = (from textbox in textBoxes
+                                 select int.TryParse(textbox.Text, out parseTemp) ? parseTemp : -1).ToArray();
+
+
+            if (!valuesInBoxes.Contains(-1))
+            {
+                double fractExcretion = CalculateFractExcretion(
+                    valuesInBoxes [0], valuesInBoxes [1], valuesInBoxes [2], valuesInBoxes [3]);
+
+                var interpreation = InterpretFractExcretion(fractExcretion);
+                Percentage.Inlines.Clear();
+                Percentage.Inlines.Add($"{fractExcretion:f2}" + "%");
+
+                Diagnosis.Inlines.Clear();
+                Diagnosis.Inlines.Add(interpreation.Item1);
+
+                Explanation.Inlines.Clear();
+                Explanation.Inlines.Add(interpreation.Item2);
+
+
+
+            }
+        }
+
+        private void Reset_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var textbox in textBoxes)
+                textbox.Text = "";
+        }
+
+        public static double CalculateFractExcretion(int SCr, int UNa, int SNa, int UCr)
+        => 100 * ((double) SCr / 1000 * UNa) / (SNa * (double) UCr / 1000);
+
+        public static (string, string) InterpretFractExcretion(double fractExcretion)
+        {
+            string oliguriaType, additionalInfo;
+
+            if (fractExcretion < 1)
+                (oliguriaType, additionalInfo) = ("Преренальна", "");
+            else if ((fractExcretion >= 1) && (fractExcretion <= 4))
+                (oliguriaType, additionalInfo) = ("Ренальна", "");
+            else
+                (oliguriaType, additionalInfo) = ("Постренальна", "");
+
+            return (oliguriaType, additionalInfo);
         }
     }
 }
